722f05708fea2e1c5ef467f9eb538896853d817c
Johan Bergens <johan.bergens@gmail.com>
2018-11-20 23:13:52 +0100

    Added very simple app3

---
+++ src/app3/root.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Root extends React.Component {
+  render() {
+    return (
+      <div style={{marginTop: '100px'}}>
+        <h2>App 3 - React again</h2>
+        This was rendered by app 3, which is written in React.
+      </div>
+    );
+  }
+}
